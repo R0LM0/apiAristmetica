d72890af1ee063e2857ba7c16959df22b9d53625
r0lm0 <rolmo92@gmail.com>
2025-05-31 22:54:20 -0600

    update proj para render

--- src/config/database.js
+++ src/config/database.js
@@ -3,15 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Crear una nueva instancia de Sequelize sin llamar a sync()
 const sequelize = new Sequelize(
-    process.env.DB_NAME,        // Nombre de la base de datos
-    process.env.DB_USER,        // Usuario de la base de datos
-    process.env.DB_PASSWORD,    // Contraseña de la base de datos
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,   // Host
-        dialect: 'postgres',         // Dialecto de base de datos
-        logging: false,              // Desactivar logging si lo prefieres
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: 5432,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // importante para Render
+            },
+        },
+        logging: false,
     }
 );
 
